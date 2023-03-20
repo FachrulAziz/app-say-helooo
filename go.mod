5537bdd2781f3d0271024aeff7934c186fc69bbf
FachrulAziz <fachrulaziz95@gmail.com>
2023-03-20 19:51:18 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/FachrulAziz/app-say-helooo
+
+go 1.20
+
+require github.com/FachrulAziz/go-say-hellooo/v2 v2.0.0 // indirect
+
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhellooo "github.com/FachrulAziz/go-say-hellooo/v2"
+)
+
+func main() {
+	fmt.Println(gosayhellooo.SayHello(" Fachrul Aziz"))
+}
